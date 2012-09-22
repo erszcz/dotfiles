6bd8c5b7fa211f7a99d6165f44c999c4f52f123d
Radosław Szymczyszyn <lavrin@gmail.com>
2012-09-22 18:17:55 +0200

    awesome: use maximized layout on www tag

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -49,10 +49,14 @@ layouts =
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+    names  = { "www", 2, 3, 4, 5, 6, 7, 8, 9 },
+    layout = { awful.layout.suit.max, layouts[1], layouts[1], layouts[1],
+               layouts[1], layouts[1], layouts[1], layouts[1], layouts[1] }
+}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag(tags.names, s, tags.layout)
 end
 -- }}}
 
