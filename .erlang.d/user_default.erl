547dd85d06715f0964be7cd475b126338a3f248a
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2013-05-08 14:09:38 +0200

    erlang: switch user_default.erl

--- .erlang.d/user_default.erl
+++ .erlang.d/user_default.erl
@@ -1,202 +1,217 @@
+%%%------------------------------------------------------------------------
+%%% File: $Id$
+%%%------------------------------------------------------------------------
+%%% @doc This is an extension of the shell commands
+%%%      to do all the work!  Either place this file in the
+%%%      path accessible to Erlang (via ERL_LIBS) or
+%%%      add this line to the ~/.erlang file:
+%%%      ``code:load_abs(os:getenv("HOME") ++ "/.erlang/user_default").''
+%%%
+%%% @author  Serge Aleynikov <saleyn@gmail.com>
+%%% @version $Revision$
+%%%          $Date$
+%%% @end
+%%%------------------------------------------------------------------------
+%%% $URL$
+%%%------------------------------------------------------------------------
+%%% ``The contents of this file are subject to the Erlang Public License,
+%%% Version 1.1, (the "License"); you may not use this file except in
+%%% compliance with the License. You should have received a copy of the
+%%% Erlang Public License along with this software. If not, it can be
+%%% retrieved via the world wide web at http://www.erlang.org/.
+%%%
+%%% Software distributed under the License is distributed on an "AS IS"
+%%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%%% the License for the specific language governing rights and limitations
+%%% under the License.
+%%%
+%%% The Initial Developer of the Original Code is Serge Aleynikov.
+%%% Portions created by Ericsson are Copyright 1999, Ericsson Utvecklings
+%%% AB. All Rights Reserved.''
+%%%------------------------------------------------------------------------
 -module(user_default).
--author('serge@hq.idt.net').
-
-%% Compile this file and use this line in your ~/.erlang file (with
-%% correct path, of course!) to where the user_default.beam file is stored.
-%%
-%%    code:load_abs("/home/fritchie/erlang/user_default").
+-author('saleyn@gmail.com').
 
 -export([help/0,dbgtc/1, dbgon/1, dbgon/2,
          dbgadd/1, dbgadd/2, dbgdel/1, dbgdel/2, dbgoff/0,
-	 dbg_ip_trace/1,
-         l/0, mm/0, la/0]).
--export([my_tracer/0, my_dhandler/2, filt_state_from_term/1]).
+         p/1, l/0, nl/0, mm/0, tc/2, tc/4]).
 
--import(io, [format/1]).
+-import(io, [format/1, format/2]).
 
 help() ->
-     shell_default:help(),
-     format("** user extended commands **~n"),
-     format("dbgtc(File)   -- use dbg:trace_client() to read data from File\n"),
-     format("dbgon(M)      -- enable dbg tracer on all funs in module M\n"),
-     format("dbgon(M,Fun)  -- enable dbg tracer for module M and function F\n"),
-     format("dbgon(M,File) -- enable dbg tracer for module M and log to File\n"),
-     format("dbgadd(M)     -- enable call tracer for module M\n"),
-     format("dbgadd(M,F)   -- enable call tracer for function M:F\n"),
-     format("dbgdel(M)     -- disable call tracer for module M\n"),
-     format("dbgdel(M,F)   -- disable call tracer for function M:F\n"),
-     format("dbgoff()      -- disable dbg tracer (calls dbg:stop/0)\n"),
-     format("l()           -- load all changed modules\n"),
-     format("la()          -- load all modules\n"),
-%     format("nl()          -- load all changed modules on all known nodes\n"),
-     format("mm()          -- list modified modules\n"),
-     true.
+    shell_default:help(),
+    format("** user extended commands **~n"),
+    format("dbgtc(File)   -- use dbg:trace_client() to read data from File\n"),
+    format("dbgon(M)      -- enable dbg tracer on all funs in module M\n"),
+    format("dbgon(M,Fun)  -- enable dbg tracer for module M and function F\n"),
+    format("dbgon(M,File) -- enable dbg tracer for module M and log to File\n"),
+    format("dbgadd(M)     -- enable call tracer for module M\n"),
+    format("dbgadd(M,F)   -- enable call tracer for function M:F\n"),
+    format("dbgdel(M)     -- disable call tracer for module M\n"),
+    format("dbgdel(M,F)   -- disable call tracer for function M:F\n"),
+    format("dbgoff()      -- disable dbg tracer (calls dbg:stop/0)\n"),
+    format("p(Term)       -- print term using io:format(\"~s\\n\", [Term])\n", ["~p"]),
+    format("l()           -- load all changed modules\n"),
+    format("nl()          -- load all changed modules on all known nodes\n"),
+    format("mm()          -- list modified modules\n"),
+    format("tc(N,M,F,A)   -- evaluate {M,F,A} N times and return {TotMkSecs, MkSecs/call, Result}\n"),
+    format("tc(N,F)       -- evaluate F N times and return {MkSecs, MkSecs/call, Result}\n"),
+    true.
+
+%% these are in alphabetic order it would be nice if they
+%% were to *stay* so!
 
 dbgtc(File) ->
-    Fun = fun({trace,_,call,{M,F,A}}, _) ->
-                 io:format("call: ~w:~w~w~n", [M,F,A]);
-             ({trace,_,return_from,{M,F,A},R}, _) ->
-                 io:format("retn: ~w:~w/~w -> ~w~n", [M,F,A,R]);
-             (A,B) ->
-                 io:format("~w: ~w~n", [A,B])
-          end,
-    dbg:trace_client(file, File, {Fun, []}).
+   Fun = fun({trace,_,call,{M,F,A}}, _)          -> io:format("call: ~w:~w~w~n", [M,F,A]);
+            ({trace,_,return_from,{M,F,A},R}, _) -> io:format("retn: ~w:~w/~w -> ~w~n", [M,F,A,R]);
+            (A,B)                                -> io:format("~w: ~w~n", [A,B]) end,
+   dbg:trace_client(file, File, {Fun, []}).
 
 dbgon(Module) ->
-    case dbg:tracer() of
-    {ok,_} ->
-       dbg:p(all,call),
-       dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
-       ok;
-    Else ->
-       Else
-    end.
+   case dbg:tracer() of
+   {ok,_} ->
+      dbg:p(all,call),
+      dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
+      ok;
+   Else ->
+      Else
+   end.
 
 dbgon(Module, Fun) when is_atom(Fun) ->
-    {ok,_} = dbg:tracer(),
-    dbg:p(all,call),
-    %%dbg:tpl(Module, Fun, [{'_',[],[{return_trace}]}]),
-    dbg:tpl(Module, Fun, [{'_',[],[{return_trace},{exception_trace}]}]),
-    ok;
+   {ok,_} = dbg:tracer(),
+   dbg:p(all,call),
+   dbg:tpl(Module, Fun, [{'_',[],[{return_trace}]}]),
+   ok;
 
 dbgon(Module, File) when is_list(File) ->
-    {ok,_} = dbg:tracer(port, dbg:trace_port(file, File)),
-    dbg:p(all,[call, running, garbage_collection, timestamp, return_to]),
-    %%dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
-    dbg:tpl(Module, [{'_',[],[{return_trace},{exception_trace}]}]),
-    ok;
-
-dbgon(Module, TcpPort) when is_integer(TcpPort) ->
-    io:format("Use this command on the node you're tracing (-remsh ...)\n"),
-    io:format("Use dbg:stop() on target node when done.\n"),
-    {ok,_} = dbg:tracer(port, dbg:trace_port(ip, TcpPort)),
-    dbg:p(all,call),
-    %%dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
-    dbg:tpl(Module, [{'_',[],[{return_trace},{exception_trace}]}]),
-    ok.
-
-dbg_ip_trace(TcpPort) ->
-    io:format("Run on same machine as target but NOT -remsh target-node\n"),
-    io:format("Use dbg:stop() when done.\n"),
-    dbg:trace_client(ip, TcpPort).
+   {ok,_} = dbg:tracer(file, dbg:trace_port(file, File)),
+   dbg:p(all,call),
+   dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
+   ok.
 
 dbgadd(Module) ->
-    %%dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
-    dbg:tpl(Module, [{'_',[],[{return_trace},{exception_trace}]}]),
-    ok.
+   dbg:tpl(Module, [{'_',[],[{return_trace}]}]),
+   ok.
 
 dbgadd(Module, Fun) ->
-    %%dbg:tpl(Module, Fun, [{'_',[],[{return_trace}]}]),
-    dbg:tpl(Module, Fun, [{'_',[],[{return_trace},{exception_trace}]}]),
-    ok.
+   dbg:tpl(Module, Fun, [{'_',[],[{return_trace}]}]),
+   ok.
 
 dbgdel(Module) ->
-    dbg:ctpl(Module),
-    ok.
+   dbg:ctpl(Module),
+   ok.
 
 dbgdel(Module, Fun) ->
-    dbg:ctpl(Module, Fun),
-    ok.
+   dbg:ctpl(Module, Fun),
+   ok.
 
 dbgoff() ->
-    dbg:stop().
+   dbg:stop().
 
-%% Reload modules that have been modified since last load.  From Tobbe
-%% Tornqvist, http://blog.tornkvist.org/, "Easy load of recompiled
-%% code", which may in turn have come from Serge?
+%% @doc Term printer
+p(Term) ->
+   io:format("~p\n", [Term]).
+
+%% @doc Load all changed modules.
 
 l() ->
-    [c:l(M) || M <- mm()].
+    lists:foreach(
+      fun(M) ->
+          io:format("Loading ~p -> ~p~n", [M, c:l(M)])
+      end,
+      mm()
+     ).
+
+%% @doc Load all changed modules on all visible nodes
+
+nl() ->
+    lists:foreach(
+      fun(M) ->
+          io:format("Network loading ~p -> ~p~n", [M, c:nl(M)])
+      end,
+      mm()
+     ).
+
+%% Vladimir Sekissov's fine code
 
 mm() ->
     modified_modules().
 
 modified_modules() ->
-    [M || {M, _} <- code:all_loaded(), 
-	  module_modified(M) == true].
+    [M || {M, _} <-  code:all_loaded(), module_modified(M) == true].
 
 module_modified(Module) ->
     case code:is_loaded(Module) of
-	{file, preloaded} ->
-	    false;
-	{file, Path} ->
-	    CompileOpts = 
-		proplists:get_value(compile, Module:module_info()),
-	    CompileTime = proplists:get_value(time, CompileOpts),
-	    Src = proplists:get_value(source, CompileOpts),
-	    module_modified(Path, CompileTime, Src);
-	_ ->
-	    false
+    {file, preloaded} ->
+        false;
+    {file, Path} ->
+        CompileOpts = proplists:get_value(compile, Module:module_info()),
+        CompileTime = proplists:get_value(time, CompileOpts),
+        Src = proplists:get_value(source, CompileOpts),
+        module_modified(Path, CompileTime, Src);
+    _ ->
+        false
     end.
 
 module_modified(Path, PrevCompileTime, PrevSrc) ->
     case find_module_file(Path) of
-	false ->
-	    false;
-	ModPath ->
-	    case beam_lib:chunks(ModPath, ["CInf"]) of
-		{ok, {_, [{_, CB}]}} ->
-		    CompileOpts =  binary_to_term(CB),
-		    CompileTime = proplists:get_value(time,                             
-						      CompileOpts),
-		    Src = proplists:get_value(source, CompileOpts),
-		    not (CompileTime == PrevCompileTime) and 
-							   (Src == PrevSrc);
-		_ ->
-		    false
-	    end
+    false ->
+        false;
+    ModPath ->
+        {ok, {_, [{_, CB}]}} = beam_lib:chunks(ModPath, ["CInf"]),
+        CompileOpts =  binary_to_term(CB),
+        CompileTime = proplists:get_value(time, CompileOpts),
+        Src = proplists:get_value(source, CompileOpts),
+        not (CompileTime == PrevCompileTime) and (Src == PrevSrc)
     end.
 
 find_module_file(Path) ->
     case file:read_file_info(Path) of
-	{ok, _} ->
-	    Path;
-	_ ->
-	    %% may be the path was changed
-	    case code:where_is_file(filename:basename(Path)) of
-		non_existing ->
-		    false;
-		NewPath ->
-		    NewPath
-	    end
+    {ok, _} ->
+        Path;
+    _ ->
+        %% maybe the path was changed?
+        %% NB: the following does not exist ... but the whole works
+        %% fairly well anyway
+        case code:where_is_file(filename:basename(Path)) of
+        non_existing ->
+            false;
+        NewPath ->
+            NewPath
+        end
     end.
 
-%% Reload all modules, regardless of age.
-la() ->
-    FiltZip = lists:filter(
-	fun({_Mod, Path}) when is_list(Path) ->
-		case string:str(Path, "/kernel-") +
-		     string:str(Path, "/stdlib-") of
-			0 -> true;
-			_ -> false
-		end;
-	    (_) -> false
-	end, code:all_loaded()),
-    {Ms, _} = lists:unzip(FiltZip),
-    lists:foldl(fun(M, Acc) ->
-			case shell_default:l(M) of
-				{error, _} -> Acc;
-				_          -> [M|Acc]
-			end
-		end, [], Ms).
+% Profiling functions inspired by Ulf Wiger post:
+% http://www.erlang.org/pipermail/erlang-questions/2007-August/028462.html
 
-my_tracer() ->
-    dbg:tracer(process, {fun my_dhandler/2, user}).
+tc(N, F) when N > 0 ->
+    time_it(fun() -> exit(call(N, N, F, erlang:now())) end).
 
-my_dhandler(TraceMsg, Acc) ->
-    dbg:dhandler(filt_state_from_term(TraceMsg), Acc).
+tc(N, M, F, A) when N > 0 ->
+    time_it(fun() -> exit(call(N, N, M, F, A, erlang:now())) end).
 
-filt_state_from_term(T) when is_tuple(T), element(1, T) == state ->
-    sTatE;
-filt_state_from_term(T) when is_tuple(T), element(1, T) == chain_r ->
-    cHain_R;
-filt_state_from_term(T) when is_tuple(T), element(1, T) == g_hash_r ->
-    g_Hash_R;
-filt_state_from_term(T) when is_tuple(T), element(1, T) == hash_r ->
-    hAsh_R;
-filt_state_from_term(T) when is_tuple(T) ->
-    list_to_tuple(filt_state_from_term(tuple_to_list(T)));
-filt_state_from_term([H|T]) ->
-    [filt_state_from_term(H)|filt_state_from_term(T)];
-filt_state_from_term(X) ->
-    X.
+time_it(F) -> 
+    Pid  = spawn_opt(F, [{min_heap_size, 16384}]),
+    MRef = erlang:monitor(process, Pid),
+    receive
+    {'DOWN', MRef, process, _, Result} -> Result
+    end.
+
+call(1, X, F, Time1) ->
+    Res = (catch F()),
+    return(X, Res, Time1, erlang:now());
+call(N, X, F, Time1) ->
+    (catch F()),
+    call(N-1, X, F, Time1).
+
+call(1, X, M, F, A, Time1) ->
+    Res = (catch apply(M, F, A)),
+    return(X, Res, Time1, erlang:now());
+call(N, X, M, F, A, Time1) ->
+    catch apply(M, F, A),
+    call(N-1, X, M, F, A, Time1).
+
+return(N, Res, Time1, Time2) ->
+    Int   = timer:now_diff(Time2, Time1),
+    {Int, Int / N, Res}.
+
