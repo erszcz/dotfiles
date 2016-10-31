333a3d5d10dfd1006df820083e9e7fd662848f84
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-10-31 12:04:23 +0100

    elixir/iex: add IEx colorscheme for a light terminal background

---
+++ .iex.exs
@@ -0,0 +1,4 @@
+IEx.configure colors: [ eval_result: [:blue],
+                        doc_inline_code: [:blue],
+                        doc_title: [:red, :reverse],
+                        doc_headings: [:red, :bright] ]
