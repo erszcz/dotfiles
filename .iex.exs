f925d007946dfc4e84f54af360fed67f70794673
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-11-04 17:49:51 +0100

    elixir/iex: make code block blue too

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,5 @@
 IEx.configure colors: [ eval_result: [:blue],
+                        doc_code: [:blue],
                         doc_inline_code: [:blue],
                         doc_title: [:red, :reverse],
                         doc_headings: [:red, :bright] ]
