724e258cc23c0fb75f9339a0015e969b7f0b980d
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-02-15 15:53:02 +0100

    Hammerspoon: define and bind left60, right40

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,8 +2,10 @@ hs.window.animationDuration = 0
 units = {
   left30        = { x = 0.00, y = 0.00, w = 0.35, h = 1.00 },
   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  left60        = { x = 0.00, y = 0.00, w = 0.60, h = 1.00 },
   left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
   right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+  right40       = { x = 0.60, y = 0.00, w = 0.40, h = 1.00 },
   right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
   right70       = { x = 0.35, y = 0.00, w = 0.65, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
@@ -22,8 +24,8 @@ hs.hotkey.bind(mash, 'l',     function() hs.window.focusedWindow():move(units.ri
 hs.hotkey.bind(mash, 'h',     function() hs.window.focusedWindow():move(units.left30,     nil, true) end)
 hs.hotkey.bind(mash, 'k',     function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
 hs.hotkey.bind(mash, 'j',     function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
-hs.hotkey.bind(mash, ']',     function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
-hs.hotkey.bind(mash, '[',     function() hs.window.focusedWindow():move(units.upleft70,   nil, true) end)
+hs.hotkey.bind(mash, ']',     function() hs.window.focusedWindow():move(units.right40,    nil, true) end)
+hs.hotkey.bind(mash, '[',     function() hs.window.focusedWindow():move(units.left60,     nil, true) end)
 hs.hotkey.bind(mash, ';',     function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
 hs.hotkey.bind(mash, "'",     function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
 hs.hotkey.bind(mash, 'm',     function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
