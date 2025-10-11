0d2092b89ba691574228a0b10496777a5c5b567d
Radek Szymczyszyn <lavrin@gmail.com>
2025-10-12 00:14:48 +0200

    hammerspoon: tweak config for a bigger display

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,13 +1,14 @@
 hs.window.animationDuration = 0
 units = {
-  left30        = { x = 0.00, y = 0.00, w = 0.35, h = 1.00 },
+  left30        = { x = 0.00, y = 0.00, w = 0.33, h = 1.00 },
   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
   left60        = { x = 0.00, y = 0.00, w = 0.60, h = 1.00 },
-  left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
-  right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+  left70        = { x = 0.00, y = 0.00, w = 0.67, h = 1.00 },
+  right30       = { x = 0.67, y = 0.00, w = 0.33, h = 1.00 },
   right40       = { x = 0.60, y = 0.00, w = 0.40, h = 1.00 },
   right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-  right70       = { x = 0.35, y = 0.00, w = 0.65, h = 1.00 },
+  right70       = { x = 0.35, y = 0.00, w = 0.67, h = 1.00 },
+  mid30         = { x = 0.33, y = 0.00, w = 0.34, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
   upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
@@ -18,14 +19,26 @@ units = {
 }
 
 mash = { 'cmd', 'ctrl', 'alt' }
+smash = { 'shift', 'cmd', 'ctrl', 'alt' }
+
 hs.hotkey.bind(mash, 'Left',  function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
 hs.hotkey.bind(mash, 'Right', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
+
 hs.hotkey.bind(mash, 'l',     function() hs.window.focusedWindow():move(units.right70,    nil, true) end)
 hs.hotkey.bind(mash, 'h',     function() hs.window.focusedWindow():move(units.left30,     nil, true) end)
 hs.hotkey.bind(mash, 'k',     function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
 hs.hotkey.bind(mash, 'j',     function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
-hs.hotkey.bind(mash, ']',     function() hs.window.focusedWindow():move(units.right40,    nil, true) end)
-hs.hotkey.bind(mash, '[',     function() hs.window.focusedWindow():move(units.left60,     nil, true) end)
 hs.hotkey.bind(mash, ';',     function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
 hs.hotkey.bind(mash, "'",     function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
+
+-- left 2/3, right 1/3
+hs.hotkey.bind(mash, '[',     function() hs.window.focusedWindow():move(units.left70,     nil, true) end)
+hs.hotkey.bind(mash, ']',     function() hs.window.focusedWindow():move(units.right30,    nil, true) end)
+
+-- left 1/3, right 2/3
+hs.hotkey.bind(smash, '[',    function() hs.window.focusedWindow():move(units.left30,     nil, true) end)
+hs.hotkey.bind(smash, ']',    function() hs.window.focusedWindow():move(units.right70,    nil, true) end)
+
+-- mid 1/3, maximum
+hs.hotkey.bind(mash, 'p',     function() hs.window.focusedWindow():move(units.mid30,      nil, true) end)
 hs.hotkey.bind(mash, 'm',     function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
